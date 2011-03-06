2c77f4eebb2471c482f2e0fafdd66019bfc6e70f
nyapoo <nyapoo@gmail.com>
2011-03-06 20:30:54 +0900

    3rd commit

--- Hello/Form1.h
+++ Hello/Form1.h
@@ -41,6 +41,9 @@ namespace Hello {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Button^  button1;
+	protected: 
 
 	private:
 		/// <summary>
@@ -55,13 +58,49 @@ namespace Hello {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(13, 13);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(131, 12);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Welcome to VC++ World!";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(15, 29);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+//				 MessageBox::Show("Welcome to VC++ World!");
+				 DateTime now = DateTime::Now;
+				 String^ msg = now.ToString();
+				 MessageBox::Show(msg);
+			 }
 	};
 }
 
